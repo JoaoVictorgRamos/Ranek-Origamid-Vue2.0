97d0d45246b02a33af1d83d6f14e9d32c4dce296
JoaoVictogRamos <joaovictorgramos12@gmail.com>
2022-08-24 10:10:35 -0300

    produto-estilo

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,16 +1,24 @@
 <template>
   <section class="produtos-container">
-    <div v-for="produto in produtos" :key="produto.id">
-      <img
-        v-if="produto.fotos"
-        :src="produto.fotos[0]"
-        :alt="produto.fotos[0].titulo"
-      />
-      <h2 class="titulo">{{ produto.nome }}</h2>
-      <p class="preco">{{ produto.preco }}</p>
-      <p class="descricao">{{ produto.descricao }}</p>
+    <div v-if="produtos && produtos.length > 0" class="produtos">
+      <div v-for="produto in produtos" :key="produto.id" class="produto">
+        <router-link to="/">
+          <img
+            v-if="produto.fotos"
+            :src="produto.fotos[0]"
+            :alt="produto.fotos[0].titulo"
+          />
+          <h2 class="titulo">{{ produto.nome }}</h2>
+          <p class="preco">{{ produto.preco }}</p>
+          <p class="descricao">{{ produto.descricao }}</p>
+        </router-link>
+      </div>
     </div>
-    {{ url }}
+    <div v-else-if="produtos && produtos.length === 0">
+      <p class="sem-resultado">
+        Busca sem resultado. Tente buscar outro termo.
+      </p>
+    </div>
   </section>
 </template>
 
@@ -49,4 +57,47 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  z-index: 1;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultado {
+  text-align: center;
+}
+</style>
