82b946cacc279e38ce1493118ce919b2d546d6d8
JoaoVictogRamos <joaovictorgramos12@gmail.com>
2022-08-25 09:23:20 -0300

    usuario-estilo

--- src/views/usuario/UsuarioView.vue
+++ src/views/usuario/UsuarioView.vue
@@ -41,4 +41,48 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.usuario {
+  display: grid;
+  grid-template-columns: minmax(140px, 200px) 1fr;
+  max-width: 900px;
+  margin: 40px auto;
+  grid-gap: 30px;
+  padding: 20px;
+}
+
+@media (max-width: 500px) {
+  .usuario {
+    grid-template-columns: 1fr;
+    margin: 0px auto;
+    grid-gap: 20px;
+    padding: 20px;
+  }
+}
+
+.sidenav a,
+.sidenav button {
+  padding: 10px;
+  display: block;
+  background: #f4f7fc;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+
+.sidenav a.router-link-exact-active,
+.sidenav a:hover,
+.sidenav button:hover {
+  background: #87f;
+  color: #fff;
+}
+
+.sidenav button {
+  border: none;
+  width: 100%;
+  font-size: 1rem;
+  text-align: left;
+  font-family: Arial, Helvetica, sans-serif;
+  cursor: pointer;
+  padding: 10px;
+}
+</style>
